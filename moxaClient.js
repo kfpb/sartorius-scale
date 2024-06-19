a9382e8bf7dbf5be71c55be6832c40ed28c2eecb
imam347 <imamazmi33@gmail.com>
2024-06-19 14:12:00 +0700

    Fix : add handling error

--- moxaClient.js
+++ moxaClient.js
@@ -1,21 +1,42 @@
 const net = require('net');
+const EventEmitter = require('events');
 
-const moxaHost = '192.168.127.254';
-const moxaPort = 4001;
-const client = new net.Socket();
+class MoxaClient extends EventEmitter {
+  constructor() {
+    super();
+    this.client = new net.Socket();
+    this.moxaHost = '192.168.127.254';
+    this.moxaPort = 4001;
 
-client.connect(moxaPort, moxaHost, () => {
-  console.log('Terhubung ke Moxa');
-});
- 
-function requestScaleData() {
-  const command = 'WI\r';
-  client.write(command);
-  setTimeout(requestScaleData, 1000);
+    this.client.connect(this.moxaPort, this.moxaHost, () => {
+      console.log('Terhubung ke Moxa');
+      this.emit('connected');
+    });
+
+    this.client.on('data', (data) => {
+      this.emit('data', data);
+    });
+
+    this.client.on('error', (error) => {
+      console.error('Terjadi kesalahan:', error);
+      this.emit('error', error);
+    });
+
+    this.client.on('close', (hadError) => {
+      if (hadError) {
+        console.log('Gagal terhubung ke Moxa');
+      } else {
+        console.log('Koneksi ke Moxa ditutup');
+      }
+      this.emit('close', hadError);
+    });
+  }
+
+  requestScaleData() {
+    const command = 'WI\r';
+    this.client.write(command);
+    setTimeout(() => this.requestScaleData(), 1000);
+  }
 }
 
-module.exports = {
-  moxaHost,
-  requestScaleData,
-  ...client
-};
+module.exports = new MoxaClient();
